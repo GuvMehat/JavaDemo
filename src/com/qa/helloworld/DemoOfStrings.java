2660235771c9aaca2ecd9d2735357a50ac601ece
guvmehat <gurveerm@hotmail.com>
2022-04-21 11:51:55 +0100

    String manipulation Exercise 1

---
+++ src/com/qa/helloworld/DemoOfStrings.java
@@ -0,0 +1,20 @@
+package com.qa.helloworld;
+
+import java.util.Locale;
+
+public class DemoOfStrings {
+
+	public static void main(String args []) {
+		String str1 = "today it is sunny";
+		String str2 = "yesterday it was raining";
+	
+		// Print out both strings in all caps
+		System.out.println(str1.toUpperCase() +", "+ str2.toUpperCase());
+	
+		// Use substring to print TODAY IT IS RANING
+		String str3 = str1.substring(0,11);
+		String str4 = str2.substring(16, 24);
+		System.out.println(str3.toUpperCase() + str4.toUpperCase());
+	
+	}
+}
